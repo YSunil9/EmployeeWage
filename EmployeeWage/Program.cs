b67f273c4d1d2e302d9dca6d953c31d7e31c13ff
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-10 15:47:38 +0530

    Calculate waages for 100 days

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,17 +6,19 @@
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
         static void Main(string[] args)
         {
             //variables
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             //computation
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
-                //computation
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
@@ -32,11 +34,10 @@
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
-
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
